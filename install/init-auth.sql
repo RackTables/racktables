7ed412d527488feb9158129b71a567acf0498eae
Denis Ovsienko <infrastation@yandex.ru>
2007-10-15 18:04:39 +0000

    r1207 + fixed comments

--- install/init-auth.sql
+++ install/init-auth.sql
@@ -3,8 +3,9 @@ INSERT INTO `UserPermission` (`user_id`, `page`, `tab`, `access`) VALUES (0,'hel
 INSERT INTO `UserAccount` (`user_id`, `user_name`, `user_enabled`, `user_password_hash`, `user_realname`)
 VALUES (1,'admin','yes',sha1(
 
-# Change the next line to to your password, e.g.:
+# Uncomment and change the next line to your password, e.g.:
 # 'mysecretpassword'
+# ... and comment out the line below:
 PLEASE_READ_THE_INSTALL_DOCUMENTATION
 
 ),'RackTables Administrator');
