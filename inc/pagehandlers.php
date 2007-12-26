8b6ffe9ca4607fe37ab20397c598b6f0a0ffd32b
Denis Ovsienko <infrastation@yandex.ru>
2007-12-26 11:12:40 +0000

    r1430 + trac#85 --- got rid of the following functions: handler_row handler_rack handler_objects handler_rackspace handler_ipv4space handler_iprange handler_accounts handler_perms handler_ui handler_dict handler_attrs handler_reports handler_help handler_ipaddress

--- inc/pagehandlers.php
+++ inc/pagehandlers.php
@@ -71,133 +71,10 @@ function assertBoolArg ($argname, $ok_if_empty = FALSE)
 	}
 }
 
-function handler_row ($tabno)
-{
-	assertUIntArg ('row_id');
-	switch ($tabno)
-	{
-		case 'default':
-			renderRow ($_REQUEST['row_id']);
-			break;
-		case 'newrack':
-			renderNewRackForm ($_REQUEST['row_id']);
-			break;
-		default:
-			showError ("Invalid tab '${tabno}' requested in handler_row().");
-	}
-}
-
-function handler_rack ($tabno)
-{
-	assertUIntArg ('rack_id');
-	switch ($tabno)
-	{
-		case 'default':
-			// FIXME: add tab renderer instead of table generator
-			renderRackPage ($_REQUEST['rack_id']);
-			break;
-		case 'problems':
-			renderRackProblems ($_REQUEST['rack_id']);
-			break;
-		case 'design':
-			renderRackDesign ($_REQUEST['rack_id']);
-			break;
-		case 'edit':
-			renderEditRackForm ($_REQUEST['rack_id']);
-			break;
-		default:
-			showError ("Invalid tab '${tabno}' requested in handler_rack().");
-	}
-}
-
-function handler_objects ($tabno)
-{
-	switch ($tabno)
-	{
-		case 'default':
-			renderObjectGroupSummary();
-			break;
-		case 'newobj':
-			renderNewObjectForm();
-			break;
-		case 'newmulti':
-			renderAddMultipleObjectsForm();
-			break;
-		default:
-			showError ("Invalid tab '${tabno}' requested in handler_objects().");
-	}
-}
-
 function handler_objgroup ()
 {
 	assertUIntArg ('group_id');
 	renderObjectGroup ($_REQUEST['group_id']);
-}
-
-function handler_rackspace ($tabno)
-{
-	switch ($tabno)
-	{
-		case 'default':
-			renderRackspace();
-			break;
-		case 'history':
-			renderRackspaceHistory ();
-			break;
-		case 'firstrow':
-			renderFirstRowForm ();
-			break;
-		default:
-			showError ("Invalid tab '${tabno}' requested in handler_rackspace().");
-	}
-}
-
-function handler_ipv4space ($tabno)
-{
-	switch ($tabno)
-	{
-		case 'default':
-			renderAddressspace();
-			break;
-		case 'newrange':
-			renderAddNewRange();
-			break;
-		default:
-			showError ("Invalid tab '${tabno}' requested in handler_ipv4space().");
-	}
-}
-
-function handler_iprange ($tabno)
-{
-	switch ($tabno)
-	{
-		case 'default':
-			renderIPRange();
-			break;
-		case 'properties':
-			renderIPRangeProperties();
-			break;
-		default:
-			showError ("Invalid tab '${tabno}' requested in handler_iprange().");
-	}
-}
-
-function handler_ipaddress ($tabno)
-{
-	switch ($tabno)
-	{
-		case 'default':
-			renderIPAddress();
-			break;
-		case 'properties':
-			renderIPAddressProperties();
-			break;
-		case 'assignment':
-			renderIPAddressAssignment();
-			break;
-		default:
-			showError ("Invalid tab '${tabno}' requested in handler_ipaddress().");
-	}
 }
 
 function handler_search ($tabno)
@@ -217,54 +94,6 @@ function handler_config ($tabno)
 	}
 }
 
-function handler_accounts ($tabno)
-{
-	switch ($tabno)
-	{
-		case 'default':
-			renderAccounts();
-			break;
-		case 'edit':
-			renderAccountsEditForm();
-			break;
-		default:
-			showError ("Invalid tab '${tabno}' requested in handler_accounts().");
-	}
-}
-
-function handler_perms ($tabno)
-{
-	switch ($tabno)
-	{
-		case 'default':
-			renderPermissions();
-			break;
-		case 'edit':
-			renderPermissionsEditForm();
-			break;
-		default:
-			showError ("Invalid tab '${tabno}' requested in handler_perms().");
-	}
-}
-
-function handler_ui ($tabno)
-{
-	switch ($tabno)
-	{
-		case 'default':
-			renderUIConfig();
-			break;
-		case 'edit':
-			renderUIConfigEditForm();
-			break;
-		case 'reset':
-			renderUIResetForm();
-			break;
-		default:
-			showError ("Invalid tab '${tabno}' requested in handler_ui().");
-	}
-}
-
 function handler_portmap ($tabno)
 {
 	switch ($tabno)
@@ -278,59 +107,6 @@ function handler_portmap ($tabno)
 		default:
 			showError ("Invalid tab '${tabno}' requested in handler_portmap().");
 	}
-}
-
-function handler_dict ($tabno)
-{
-	switch ($tabno)
-	{
-		case 'default':
-			renderDictionary();
-			break;
-		case 'edit':
-			renderDictionaryEditor();
-			break;
-		case 'chapters':
-			renderChaptersEditor();
-			break;
-		default:
-			showError ("Invalid tab '${tabno}' requested in handler_dict().");
-	}
-}
-
-function handler_attrs ($tabno)
-{
-	switch ($tabno)
-	{
-		case 'default':
-			renderAttributes();
-			break;
-		case 'editattrs':
-			renderEditAttributesForm ();
-			break;
-		case 'editmap':
-			renderEditAttrMapForm ();
-			break;
-		default:
-			showError ("Invalid tab '${tabno}' requested in handler_attrs().");
-	}
-}
-
-function handler_reports ($tabno)
-{
-	switch ($tabno)
-	{
-		case 'default':
-			renderReportSummary();
-			break;
-		default:
-			showError ("Invalid tab '${tabno}' requested in handler_reports().");
-	}
-}
-
-function handler_help ($tabno)
-{
-	renderHelpTab ($tabno);
 }
 
 ?>
