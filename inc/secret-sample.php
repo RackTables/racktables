32de40690e3caea14e399d087f5bf501415a3c88
Denis Ovsienko <infrastation@yandex.ru>
2008-01-30 17:34:47 +0000

    r1627 + add LDAP options

--- inc/secret-sample.php
+++ inc/secret-sample.php
@@ -1,12 +1,16 @@
 <?php
 /*
 *
-*  This is RackTables secret configuration file.
+*  RackTables secrets are here
 *
 */
 
 $pdo_dsn = 'mysql:host=localhost;dbname=racktables';
 $db_username = 'username';
 $db_password = 'password';
+
+// This is only necessary for 'ldap' USER_AUTH_SRC
+$ldap_server = 'some.server';
+$ldap_domain = 'some.domain';
 
 ?>
