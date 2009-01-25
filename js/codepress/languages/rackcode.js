7196af61dbad4333dde7c562ccfd65a111bc71c6
Denis Ovsienko <infrastation@yandex.ru>
2009-01-25 20:57:00 +0000

    r2367  - list new keywords in the highlighter

--- js/codepress/languages/rackcode.js
+++ js/codepress/languages/rackcode.js
@@ -4,6 +4,6 @@ Language.shortcuts = []
 Language.syntax = [
 	{ input : /{(.*?)(}|<br>|<\/P>)/g, output : '<s>{$1$2</s>'}, // tags
 	{ input : /\[(.*?)(\]|<br>|<\/P>)/g, output : '<s>[$1$2</s>'}, // predicatess
-	{ input : /\b(define|allow|deny|and|not|or)\b/g, output : '<b>$1</b>'}, // keywords
+	{ input : /\b(define|allow|deny|and|not|or|context|clear|insert|remove|on)\b/g, output : '<b>$1</b>'}, // keywords
 	{ input : /([^:]|^)#(.*?)(<br|<\/P)/g, output : '$1<i>#$2</i>$3'} // comments //	
 ]
