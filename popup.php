38ee0db8f43b7b2a7ec16b3084e8153037392ec1
Denis Yeldandi <dyeldandi@gmail.com>
2009-02-22 08:45:40 +0000

    r2472 See ticket#191

--- popup.php
+++ popup.php
@@ -1,9 +1,11 @@
+<?php
+	require 'inc/init.php';
+?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en" style="height: 100%;">
 <head>
  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 <?php
-	require 'inc/init.php';
 	// This is our context.
 	$pageno = 'objects';
 	$tabno = 'default';
