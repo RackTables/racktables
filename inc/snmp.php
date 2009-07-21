95bec9dd86f82329932c83bd5b5ad273e62e477d
Denis Ovsienko <infrastation@yandex.ru>
2009-07-21 10:50:47 +0000

    r2989  - doSNMPMining(): adjust data for chassis devices

--- inc/snmp.php
+++ inc/snmp.php
@@ -10,12 +10,21 @@ $iftable_processors['catalyst-any-100TX'] = array
 	'try_next_proc' => FALSE,
 );
 
-$iftable_processors['catalyst-25-to-26-100FX/MT-RJ'] = array
+$iftable_processors['catalyst-chassis-any-100TX'] = array
 (
-	'pattern' => '@^FastEthernet([[:digit:]]+/(25|26))$@',
-	'replacement' => 'fa\\1',
+	'pattern' => '@^FastEthernet([[:digit:]]+/)?([[:digit:]]+)$@',
+	'replacement' => 'fa\\1\\2',
+	'dict_key' => 19,
+	'label' => '\\2X',
+	'try_next_proc' => FALSE,
+);
+
+$iftable_processors['catalyst-chassis-25-to-26-100FX/MT-RJ'] = array
+(
+	'pattern' => '@^FastEthernet([[:digit:]]+/)?(25|26)$@',
+	'replacement' => 'fa\\1\\2',
 	'dict_key' => 1083,
-	'label' => '\\1',
+	'label' => '\\2',
 	'try_next_proc' => FALSE,
 );
 
@@ -25,6 +34,15 @@ $iftable_processors['catalyst-any-1000T'] = array
 	'replacement' => 'gi\\1',
 	'dict_key' => 24,
 	'label' => '\\1X',
+	'try_next_proc' => FALSE,
+);
+
+$iftable_processors['catalyst-chassis-any-1000T'] = array
+(
+	'pattern' => '@^GigabitEthernet([[:digit:]]+/)?([[:digit:]]+)$@',
+	'replacement' => 'gi\\1\\2',
+	'dict_key' => 24,
+	'label' => '\\2X',
 	'try_next_proc' => FALSE,
 );
 
@@ -55,21 +73,21 @@ $iftable_processors['catalyst-any-1000GBIC'] = array
 	'try_next_proc' => FALSE,
 );
 
-$iftable_processors['catalyst-21-to-24-combo-1000SFP'] = array
+$iftable_processors['catalyst-chassis-21-to-24-combo-1000SFP'] = array
 (
-	'pattern' => '@^GigabitEthernet([[:digit:]]+/(21|22|23|24))$@',
-	'replacement' => 'gi\\1',
+	'pattern' => '@^GigabitEthernet([[:digit:]]+/)?(21|22|23|24)$@',
+	'replacement' => 'gi\\1\\2',
 	'dict_key' => 1077,
-	'label' => '\\1',
+	'label' => '\\2',
 	'try_next_proc' => TRUE,
 );
 
-$iftable_processors['catalyst-45-to-48-combo-1000SFP'] = array
+$iftable_processors['catalyst-chassis-45-to-48-combo-1000SFP'] = array
 (
-	'pattern' => '@^GigabitEthernet([[:digit:]]+/(45|46|47|48))$@',
-	'replacement' => 'gi\\1',
+	'pattern' => '@^GigabitEthernet([[:digit:]]+/)?(45|46|47|48)$@',
+	'replacement' => 'gi\\1\\2',
 	'dict_key' => 1077,
-	'label' => '\\1',
+	'label' => '\\2',
 	'try_next_proc' => TRUE,
 );
 
@@ -88,6 +106,15 @@ $iftable_processors['catalyst-25-to-28-1000SFP'] = array
 	'replacement' => 'gi\\1',
 	'dict_key' => 1077,
 	'label' => '\\1',
+	'try_next_proc' => FALSE,
+);
+
+$iftable_processors['catalyst-chassis-25-to-28-1000SFP'] = array
+(
+	'pattern' => '@^GigabitEthernet([[:digit:]]+/)?(25|26|27|28)$@',
+	'replacement' => 'gi\\1\\2',
+	'dict_key' => 1077,
+	'label' => '\\2',
 	'try_next_proc' => FALSE,
 );
 
@@ -172,13 +199,31 @@ $iftable_processors['procurve-49-to-50-1000T'] = array
 	'try_next_proc' => FALSE,
 );
 
-$iftable_processors['netgear-any-1000T'] = array
+$iftable_processors['netgear-chassis-any-1000T'] = array
 (
 	'pattern' => '@^Unit: 1 Slot: 0 Port: ([[:digit:]]+) Gigabit - Level$@',
 	'replacement' => '\\1',
 	'dict_key' => 24,
 	'label' => '\\1',
 	'try_next_proc' => FALSE,
+);
+
+$iftable_processors['netgear-chassis-21-to-24-1000Tcombo'] = array
+(
+	'pattern' => '@^Unit: 1 Slot: 0 Port: ([[:digit:]]+) Gigabit - Level$@',
+	'replacement' => '\\1',
+	'dict_key' => 24,
+	'label' => '\\1T',
+	'try_next_proc' => FALSE,
+);
+
+$iftable_processors['netgear-chassis-21-to-24-1000SFP'] = array
+(
+	'pattern' => '@^Unit: 1 Slot: 0 Port: ([[:digit:]]+) Gigabit - Level$@',
+	'replacement' => '\\1',
+	'dict_key' => 1077,
+	'label' => '\\1F',
+	'try_next_proc' => TRUE,
 );
 
 $iftable_processors['nortel-any-1000T'] = array
@@ -196,109 +241,109 @@ $known_switches = array // key is system OID w/o "enterprises" prefix
 	(
 		'dict_key' => 380,
 		'text' => 'WS-C2950-24: 24 RJ-45/10-100TX',
-		'processors' => array ('catalyst-any-100TX'),
+		'processors' => array ('catalyst-chassis-any-100TX'),
 	),
 	'9.1.325' => array
 	(
 		'dict_key' => 382,
 		'text' => 'WS-C2950C-24: 24 RJ-45/10-100TX + 2 MT-RJ/100FX fiber',
-		'processors' => array ('catalyst-25-to-26-100FX/MT-RJ', 'catalyst-any-100TX'),
+		'processors' => array ('catalyst-chassis-25-to-26-100FX/MT-RJ', 'catalyst-chassis-any-100TX'),
 	),
 	'9.1.696' => array
 	(
 		'dict_key' => 167,
 		'text' => 'WS-C2960G-24TC-L: 20 RJ-45/10-100-1000T(X) + 4 combo-gig',
-		'processors' => array ('catalyst-21-to-24-combo-1000SFP', 'catalyst-any-1000T'),
+		'processors' => array ('catalyst-chassis-21-to-24-combo-1000SFP', 'catalyst-chassis-any-1000T'),
 	),
 	'9.1.697' => array
 	(
 		'dict_key' => 166,
 		'text' => 'WS-C2960G-48TC-L: 44 RJ-45/10-100-1000T(X) + 4 combo-gig',
-		'processors' => array ('catalyst-45-to-48-combo-1000SFP', 'catalyst-any-1000T'),
+		'processors' => array ('catalyst-chassis-45-to-48-combo-1000SFP', 'catalyst-chassis-any-1000T'),
 	),
 	'9.1.716' => array
 	(
 		'dict_key' => 164,
 		'text' => 'WS-C2960-24TT-L: 24 RJ-45/10-100TX + 2 RJ-45/10-100-1000T(X)',
-		'processors' => array ('catalyst-any-100TX', 'catalyst-any-1000T'),
+		'processors' => array ('catalyst-chassis-any-100TX', 'catalyst-chassis-any-1000T'),
 	),
 	'9.1.717' => array
 	(
 		'dict_key' => 162,
 		'text' => 'WS-C2960-48TT-L: 48 RJ-45/10-100TX + 2 RJ-45/10-100-1000T(X)',
-		'processors' => array ('catalyst-any-100TX', 'catalyst-any-1000T'),
+		'processors' => array ('catalyst-chassis-any-100TX', 'catalyst-chassis-any-1000T'),
 	),
 	'9.1.527' => array
 	(
 		'dict_key' => 210,
 		'text' => 'WS-C2970G-24T: 24 RJ-45/10-100-1000T(X)',
-		'processors' => array ('catalyst-any-1000T'),
+		'processors' => array ('catalyst-chassis-any-1000T'),
 	),
 	'9.1.561' => array
 	(
 		'dict_key' => 115,
 		'text' => 'WS-C2970G-24TS: 24 RJ-45/10-100-1000T(X) + 4 SFP/1000',
-		'processors' => array ('catalyst-25-to-28-1000SFP', 'catalyst-any-1000T'),
+		'processors' => array ('catalyst-chassis-25-to-28-1000SFP', 'catalyst-chassis-any-1000T'),
 	),
 	'9.1.633' => array
 	(
 		'dict_key' => 169,
 		'text' => 'WS-C3560-24TS: 24 RJ-45/10-100TX + 2 SFP/1000',
-		'processors' => array ('catalyst-any-1000SFP', 'catalyst-any-100TX'),
+		'processors' => array ('catalyst-any-1000SFP', 'catalyst-chassis-any-100TX'),
 	),
 	'9.1.634' => array
 	(
 		'dict_key' => 170,
 		'text' => 'WS-C3560-48TS: 48 RJ-45/10-100TX + 4 SFP/1000',
-		'processors' => array ('catalyst-any-1000SFP', 'catalyst-any-100TX'),
+		'processors' => array ('catalyst-any-1000SFP', 'catalyst-chassis-any-100TX'),
 	),
 	'9.1.563' => array
 	(
 		'dict_key' => 171,
 		'text' => 'WS-C3560-24PS: 24 RJ-45/10-100TX + 2 SFP/1000',
-		'processors' => array ('catalyst-any-1000SFP', 'catalyst-any-100TX'),
+		'processors' => array ('catalyst-any-1000SFP', 'catalyst-chassis-any-100TX'),
 	),
 	'9.1.564' => array
 	(
 		'dict_key' => 172,
 		'text' => 'WS-C3560-48PS: 48 RJ-45/10-100TX + 4 SFP/1000',
-		'processors' => array ('catalyst-any-1000SFP', 'catalyst-any-100TX'),
+		'processors' => array ('catalyst-any-1000SFP', 'catalyst-chassis-any-100TX'),
 	),
 	'9.1.516' => array
 	(
 		'dict_key' => 179,
 		'text' => 'WS-C3750-xxPS: 24 or 48 RJ-45/10-100TX + 4 SFP/1000',
-		'processors' => array ('catalyst-any-1000SFP', 'catalyst-any-100TX'),
+		'processors' => array ('catalyst-any-1000SFP', 'catalyst-chassis-any-100TX'),
 	),
 	'9.1.614' => array
 	(
 		'dict_key' => 175,
 		'text' => 'WS-C3560G-24PS: 24 RJ-45/10-100-1000T(X) + 4 SFP/1000',
-		'processors' => array ('catalyst-25-to-28-1000SFP', 'catalyst-any-1000T'),
+		'processors' => array ('catalyst-25-to-28-1000SFP', 'catalyst-chassis-any-1000T'),
 	),
 	'9.1.615' => array
 	(
 		'dict_key' => 173,
 		'text' => 'WS-C3560G-24TS: 24 RJ-45/10-100-1000T(X) + 4 SFP/1000',
-		'processors' => array ('catalyst-25-to-28-1000SFP', 'catalyst-any-1000T'),
+		'processors' => array ('catalyst-25-to-28-1000SFP', 'catalyst-chassis-any-1000T'),
 	),
 	'9.1.616' => array
 	(
 		'dict_key' => 176,
 		'text' => 'WS-C3560G-48PS: 48 RJ-45/10-100-1000T(X) + 4 SFP/1000',
-		'processors' => array ('catalyst-49-to-52-1000SFP', 'catalyst-any-1000T'),
+		'processors' => array ('catalyst-49-to-52-1000SFP', 'catalyst-chassis-any-1000T'),
 	),
 	'9.1.617' => array
 	(
 		'dict_key' => 174,
 		'text' => 'WS-C3560G-48TS: 48 RJ-45/10-100-1000T(X) + 4 SFP/1000',
-		'processors' => array ('catalyst-49-to-52-1000SFP', 'catalyst-any-1000T'),
+		'processors' => array ('catalyst-49-to-52-1000SFP', 'catalyst-chassis-any-1000T'),
 	),
 	'9.1.624' => array
 	(
 		'dict_key' => 143,
 		'text' => 'WS-C3750G-24TS: 24 RJ-45/10-100-1000T(X) + 4 SFP/1000',
-		'processors' => array ('catalyst-25-to-28-1000SFP', 'catalyst-any-1000T'),
+		'processors' => array ('catalyst-25-to-28-1000SFP', 'catalyst-chassis-any-1000T'),
 	),
 	'9.1.626' => array
 	(
@@ -316,19 +361,19 @@ $known_switches = array // key is system OID w/o "enterprises" prefix
 	(
 		'dict_key' => 389,
 		'text' => 'WS-C2950G-24: 24 RJ-45/10-100TX + 2 GBIC/1000',
-		'processors' => array ('catalyst-any-1000GBIC', 'catalyst-any-100TX'),
+		'processors' => array ('catalyst-any-1000GBIC', 'catalyst-chassis-any-100TX'),
 	),
 	'9.1.429' => array
 	(
 		'dict_key' => 390,
 		'text' => 'WS-C2950G-48: 48 RJ-45/10-100TX + 2 GBIC/1000',
-		'processors' => array ('catalyst-any-1000GBIC', 'catalyst-any-100TX'),
+		'processors' => array ('catalyst-any-1000GBIC', 'catalyst-chassis-any-100TX'),
 	),
 	'9.1.559' => array
 	(
 		'dict_key' => 387,
 		'text' => 'WS-C2950T-48: 48 RJ-45/10-100TX + 2 RJ-45/10-100-1000T(X)',
-		'processors' => array ('catalyst-any-1000T', 'catalyst-any-100TX'),
+		'processors' => array ('catalyst-chassis-any-1000T', 'catalyst-chassis-any-100TX'),
 	),
 	'9.1.749' => array
 	(
@@ -375,8 +420,8 @@ $known_switches = array // key is system OID w/o "enterprises" prefix
 	'4526.100.2.2' => array
 	(
 		'dict_key' => 562,
-		'text' => 'GSM7224: 24 RJ-45/10-100-1000T(X)',
-		'processors' => array ('netgear-any-1000T'),
+		'text' => 'GSM7224: 20 RJ-45/10-100-1000T(X) + 4 combo-gig',
+		'processors' => array ('netgear-chassis-21-to-24-1000SFP', 'netgear-chassis-21-to-24-1000Tcombo', 'netgear-chassis-any-1000T'),
 	),
 	'45.3.68.5' => array
 	(
@@ -646,10 +691,12 @@ function doSNMPmining ($object_id, $community)
 			'12.2' => 252,
 		);
 		updateStickerForCell ($objectInfo, 5, $exact_release);
-		updateStickerForCell ($objectInfo, 4, $ios_codes[$major_line]);
+		if (array_key_exists ($major_line, $ios_codes))
+			updateStickerForCell ($objectInfo, 4, $ios_codes[$major_line]);
 		$sysChassi = snmpget ($endpoints[0], $community, '1.3.6.1.4.1.9.3.6.3.0');
 		if ($sysChassi !== FALSE or $sysChassi !== NULL)
 			updateStickerForCell ($objectInfo, 1, str_replace ('"', '', substr ($sysChassi, strlen ('STRING: '))));
+		commitAddPort ($object_id, 'con0', 29, 'console', ''); // RJ-45 RS-232 console
 		$log = mergeLogs ($log, oneLiner (81, array ('catalyst-generic')));
 		break;
 	case preg_match ('/^9\.12\.3\.1\.3\./', $sysObjectID): // Nexus
@@ -660,7 +707,8 @@ function doSNMPmining ($object_id, $community)
 			'4.0' => 963,
 			'4.1' => 964,
 		);
-		updateStickerForCell ($objectInfo, 4, $nxos_codes[$major_line]);
+		if (array_key_exists ($major_line, $nxos_codes))
+			updateStickerForCell ($objectInfo, 4, $nxos_codes[$major_line]);
 		updateStickerForCell ($objectInfo, 5, $exact_release);
 		$log = mergeLogs ($log, oneLiner (81, array ('nexus-generic')));
 		break;
@@ -668,6 +716,10 @@ function doSNMPmining ($object_id, $community)
 		$exact_release = ereg_replace ('^.* revision ([^ ]+), .*$', '\\1', $sysDescr);
 		updateStickerForCell ($objectInfo, 5, $exact_release);
 		$log = mergeLogs ($log, oneLiner (81, array ('procurve-generic')));
+		break;
+	case preg_match ('/^4526\.100\.2\./', $sysObjectID): // NETGEAR
+		commitAddPort ($object_id, 'console', 681, 'console', ''); // DB-9 RS-232 console
+		$log = mergeLogs ($log, oneLiner (81, array ('netgear-generic')));
 		break;
 	default: // Nortel, NETGEAR...
 		break;
@@ -706,8 +758,6 @@ function doSNMPmining ($object_id, $community)
 	foreach ($ifInfo as $iface)
 		foreach ($known_switches[$sysObjectID]['processors'] as $processor_name)
 		{
-//			echo "subject == '" . $iface['ifDescr'] . "'<br>";
-//			echo "pattern == '" . $iftable_processors[$processor_name]['pattern'] . "', replacement == '" . $iftable_processors[$processor_name]['replacement'] . "'<br>";
 			$newname = preg_replace ($iftable_processors[$processor_name]['pattern'], $iftable_processors[$processor_name]['replacement'], $iface['ifDescr'], 1, $count);
 			if (!$count)
 				continue; // try next processor on current port
