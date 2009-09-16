68589333a110eda0b490c4aa821aa753ba52ca71
Denis Ovsienko <infrastation@yandex.ru>
2009-09-16 08:54:29 +0000

    r3133  - pre-release justifications

--- inc/config.php
+++ inc/config.php
@@ -11,7 +11,7 @@
 
 
 // Current code version is subject to change with each new release.
-define ('CODE_VERSION', '0.17.4');
+define ('CODE_VERSION', '0.17.5');
 define ('CHAP_OBJTYPE', 1);
 define ('CHAP_PORTTYPE', 2);
 
