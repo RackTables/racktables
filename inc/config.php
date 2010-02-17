fa9c9050614bb8d6066146a334eff2563fa5bddf
Denis Ovsienko <infrastation@yandex.ru>
2010-02-17 18:11:11 +0000

    r3278 pull the last maintenance commit into trunk

--- inc/config.php
+++ inc/config.php
@@ -11,7 +11,7 @@
 
 
 // Current code version is subject to change with each new release.
-define ('CODE_VERSION', '0.17.8');
+define ('CODE_VERSION', '0.17.9');
 define ('CHAP_OBJTYPE', 1);
 define ('CHAP_PORTTYPE', 2);
 
