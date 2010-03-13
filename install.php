288b9091eed6b6fc51a56c606ecf5a7900cd45e6
Denis Ovsienko <infrastation@yandex.ru>
2010-03-13 11:43:09 +0000

    r3344 copy tested changeset 3340 to maintenance

--- install.php
+++ install.php
@@ -35,11 +35,11 @@ if ($step > count ($stepfunc))
 }
 $title = "RackTables installation: step ${step} of " . count ($stepfunc);
 require_once ('inc/dictionary.php');
+header ('Content-Type: text/html; charset=UTF-8');
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
 <head><title><?php echo $title; ?></title>
-<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 <link rel=stylesheet type='text/css' href=pi.css />
 </head>
 <body>
