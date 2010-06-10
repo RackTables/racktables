2cf36003caa334041eb63c0d0a2215e4b928f6e8
Denis Ovsienko <infrastation@yandex.ru>
2010-06-10 12:22:52 +0000

    r3786 fix page title

--- upgrade.php
+++ upgrade.php
@@ -837,7 +837,7 @@ switch ($user_auth_src)
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
-<head><title>RackTable version upgrade</title>
+<head><title>RackTables upgrade script</title>
 <link rel=stylesheet type='text/css' href='css/pi.css' />
 </head>
 <body>
