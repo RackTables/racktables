d423bb7db22585c27501e65c2737d11c7042c04d
Denis Ovsienko <infrastation@yandex.ru>
2010-06-17 15:06:19 +0000

    r3842 generatePortsForCatModule(): add 3750G-16TD case

--- inc/snmp.php
+++ inc/snmp.php
@@ -1176,6 +1176,15 @@ function generatePortsForCatModule ($object_id, $slotno = 1, $mtype = 'X6748', $
 			$mac_address = nextMACAddress ($mac_address);
 		}
 		break;
+	case '3750G-16TD':
+		$mac_address = nextMACAddress ($mac_address);
+		for ($i = 1; $i <= 16; $i++)
+		{
+			commitAddPort ($object_id, "gi${slotno}/0/${i}", '1-24', "unit ${slotno} port ${i}", $mac_address);
+			$mac_address = nextMACAddress ($mac_address);
+		}
+		commitAddPort ($object_id, "te${slotno}/0/1", '5-1079', "unit ${slotno} port ${i}", $mac_address);
+		break;
 	}
 }
 
