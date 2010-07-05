49f7d5405e8333301a0b7c36a952f4797241ec53
Denis Ovsienko <infrastation@yandex.ru>
2010-07-05 15:13:59 +0000

    r3891 addBulkPorts(): fix assertions code and clean up

--- inc/interface.php
+++ inc/interface.php
@@ -1643,7 +1643,6 @@ function showMessageOrError ()
 				189 => array ('code' => 'error', 'format' => "Unknown OID '%s'"),
 				190 => array ('code' => 'error', 'format' => "Invalid VLAN ID '%s'"),
 				191 => array ('code' => 'error', 'format' => "deploy was blocked due to conflicting configuration versions"),
-				192 => array ('code' => 'error', 'format' => "You have to supply all the information for the bulk port function to be successful."), 
 
 // records 200~299 with warnings
 				200 => array ('code' => 'warning', 'format' => '%s'),
