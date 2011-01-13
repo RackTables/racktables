c9538c890832f6e235b50622c318ac8bf06fa0bb
Denis Ovsienko <infrastation@yandex.ru>
2011-01-13 10:55:16 +0000

    r4120 added necessary OB and exceptions wrapping

--- download.php
+++ download.php
@@ -1,23 +1,32 @@
 <?php
-require 'inc/init.php';
-// purely for renderAccessDenied()
-require 'inc/interface.php';
+ob_start();
+try {
+	require 'inc/init.php';
+	// purely for renderAccessDenied()
+	require 'inc/interface.php';
 
-assertUIntArg ('file_id');
-$pageno = 'file';
-$tabno = 'download';
-fixContext();
-if (!permitted())
-	renderAccessDenied();
+	assertUIntArg ('file_id');
+	$pageno = 'file';
+	$tabno = 'download';
+	fixContext();
+	if (!permitted())
+		renderAccessDenied();
 
-$asattach = (isset ($_REQUEST['asattach']) and $_REQUEST['asattach'] == 'no') ? FALSE : TRUE;
-$file = getFile($_REQUEST['file_id']);
-if ($file != NULL) 
+	$asattach = (isset ($_REQUEST['asattach']) and $_REQUEST['asattach'] == 'no') ? FALSE : TRUE;
+	$file = getFile ($_REQUEST['file_id']);
+	if ($file != NULL) 
+	{
+		header("Content-Type: {$file['type']}");
+		header("Content-Length: {$file['size']}");
+		if ($asattach)
+			header("Content-Disposition: attachment; filename={$file['name']}");
+		echo $file['contents'];
+	}
+	ob_end_flush();
+}
+catch (Exception $e)
 {
-	header("Content-Type: {$file['type']}");
-	header("Content-Length: {$file['size']}");
-	if ($asattach)
-		header("Content-Disposition: attachment; filename={$file['name']}");
-	echo $file['contents'];
+	ob_end_clean();
+	printException($e);
 }
 ?>
