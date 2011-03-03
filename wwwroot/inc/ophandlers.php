6df00a5661cb5f545cebd3925de742b45297fa57
Denis Ovsienko <infrastation@yandex.ru>
2011-03-03 16:37:24 +0000

    r4282 addRack(): use the right message code

--- wwwroot/inc/ophandlers.php
+++ wwwroot/inc/ophandlers.php
@@ -1937,7 +1937,7 @@ function submitSLBConfig ()
 	return buildRedirectURL (__FUNCTION__, 'OK', array ('slbconfig'));
 }
 
-$msgcode['addRack']['OK'] = 51;
+$msgcode['addRack']['OK'] = 48;
 $msgcode['addRack']['ERR2'] = 172;
 function addRack ()
 {
