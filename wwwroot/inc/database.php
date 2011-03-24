76bf8846a4a9aad7c4b02806fb0c1a2a2c205628
Alexey Andriyanov <alan@al-an.info>
2011-03-24 09:53:57 +0000

    r4384 getDictStats: added two new chapters into list

--- wwwroot/inc/database.php
+++ wwwroot/inc/database.php
@@ -2229,7 +2229,7 @@ function commitReduceOPC ($parent_objtype_id, $child_objtype_id)
 
 function getDictStats ()
 {
-	$stock_chapters = array (1, 2, 11, 12, 13, 14, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33);
+	$stock_chapters = array (1, 2, 11, 12, 13, 14, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35);
 	$result = usePreparedSelectBlade
 	(
 		"select Chapter.id as chapter_no, Chapter.name as chapter_name, count(dict_key) as wc from " .
