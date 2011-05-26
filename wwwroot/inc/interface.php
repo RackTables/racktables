c8eaa04dff794d52a85529f273660bf6b242cd1d
Denis Ovsienko <infrastation@yandex.ru>
2011-05-26 14:28:38 +0000

    r4522 dynamic_title_decoder(): fix HTML TITLE in 'ipaddress' and 'ipv6address' cases

--- wwwroot/inc/interface.php
+++ wwwroot/inc/interface.php
@@ -7071,14 +7071,14 @@ function dynamic_title_decoder ($path_position)
 		$address = getIPv4Address ($_REQUEST['ip']);
 		return array
 		(
-			'name' => niftyString ($_REQUEST['ip'] . ($address['name'] != '' ? ' (' . $address['name'] . ')' : '')),
+			'name' => niftyString ($_REQUEST['ip'] . ($address['name'] != '' ? ' (' . $address['name'] . ')' : ''), 50, FALSE),
 			'params' => array ('ip' => $_REQUEST['ip'])
 		);
 	case 'ipv6address':
 		$address = getIPv6Address (assertIPArg ('ip'));
 		return array
 		(
-			'name' => niftyString ($address['ip'] . ($address['name'] != '' ? ' (' . $address['name'] . ')' : '')),
+			'name' => niftyString ($address['ip'] . ($address['name'] != '' ? ' (' . $address['name'] . ')' : ''), 50, FALSE),
 			'params' => array ('ip' => $address['ip'])
 		);
 	case 'ipv4net':
