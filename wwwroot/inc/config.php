86eaaa67e0e53f9e9da6680f86cafeec5e90f666
Denis Ovsienko <infrastation@yandex.ru>
2011-08-01 13:46:59 +0000

    r4705 declare trunk to be 0.19.7

--- wwwroot/inc/config.php
+++ wwwroot/inc/config.php
@@ -13,7 +13,7 @@ committers' copies) can run into issues:
    have already been executed.
 */
 
-define ('CODE_VERSION', '0.19.6');
+define ('CODE_VERSION', '0.19.7');
 
 $max_dict_key = array
 (
@@ -44,7 +44,7 @@ $max_dict_key = array
 	'0.19.4' => 1559,
 	'0.19.5' => 1559,
 	'0.19.6' => 1559,
-	'0.19.7' => 1589,
+	'0.19.7' => 1590,
 	'0.20.0' => 1590,
 );
 
