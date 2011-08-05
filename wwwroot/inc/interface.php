cf3ecdfd1c17e61053721cbfd849881c9bbb1094
Alexey Andriyanov <alan@al-an.info>
2011-08-05 11:58:51 +0000

    r4710 bugfix: UI: removed integer hint from priority field description
    renderSLBTriplets: changed prio field description

--- wwwroot/inc/interface.php
+++ wwwroot/inc/interface.php
@@ -5309,7 +5309,7 @@ function renderSLBTriplets ($realm1, $realm2, $triplets, $display_count) {
 			echo "<textarea name=rsconfig rows=5 cols=70>${slb['rsconfig']}</textarea><br>RS config &uarr;";
 			$prio_id = "prio-$i";
 			$prio_value = htmlspecialchars($slb['prio']);
-			echo "<div style='float:left; margin-top:10px'><input name=prio type=text size=10 id=\"$prio_id\" value=\"$prio_value\"><label for=\"$prio_id\"> &larr; Priority (integer)</label></div>";
+			echo "<div style='float:left; margin-top:10px'><input name=prio type=text size=10 id=\"$prio_id\" value=\"$prio_value\"><label for=\"$prio_id\"> &larr; Priority</label></div>";
 			echo '</td></tr></form>';
 			$order = $nextorder[$order];
 		}
