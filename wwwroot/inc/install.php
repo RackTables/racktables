a0a637a6646a21d16ee6bc25a6570bbebc01d348
Denis Ovsienko <infrastation@yandex.ru>
2011-08-11 16:07:50 +0000

    r4739 Remove most of the comments from generated secret.php and provide a link to wiki instead.

--- wwwroot/inc/install.php
+++ wwwroot/inc/install.php
@@ -164,49 +164,28 @@ function init_config ()
 		echo "Error: failed to open $path_to_secret_php for writing";
 		return FALSE;
 	}
-	fwrite ($conf, "<?php\n/* This file has been generated automatically by RackTables installer.\n");
-	fwrite ($conf, " * you shouldn't normally edit it unless your database setup has changed.\n");
-	fwrite ($conf, " */\n");
+	fwrite ($conf, "<?php\n# This file has been generated automatically by RackTables installer.\n");
 	fwrite ($conf, "\$pdo_dsn = '${pdo_dsn}';\n");
 	fwrite ($conf, "\$db_username = '" . $_REQUEST['mysql_username'] . "';\n");
 	fwrite ($conf, "\$db_password = '" . $_REQUEST['mysql_password'] . "';\n\n");
 	fwrite ($conf, <<<ENDOFTEXT
-// Default setting is to authenticate users locally, but it is possible to
-// employ existing LDAP or Apache userbase. Uncommenting below two lines MAY
-// help in switching authentication to LDAP completely.
-// More info: http://sourceforge.net/apps/mediawiki/racktables/index.php?title=RackTablesAdminGuide
-#\$user_auth_src = 'ldap';
-#\$require_local_account = FALSE;
 
-// This is only necessary for 'ldap' authentication source
-\$LDAP_options = array
-(
-	'server' => 'some.server',
-	'domain' => 'some.domain',
-#	'search_dn' => 'ou=people,O=YourCompany',
-	'search_attr' => 'uid',
-#	'displayname_attrs' => 'givenname familyname',
+\$user_auth_src = 'database';
+\$require_local_account = TRUE;
+# Default setting is to authenticate users locally, but it is possible to
+# employ existing LDAP or Apache user accounts. Check RackTables wiki for
+# more information, in particular, this page for LDAP configuration details:
+# http://sourceforge.net/apps/mediawiki/racktables/index.php?title=LDAP
 
-// LDAP cache, values in seconds. Refresh, retry and expiry values are
-// treated exactly as those for DNS SOA record. Example values 300-15-600:
-// unconditionally remeber successful auth for 5 minutes, after that still
-// permit user access, but try to revalidate username and password on the
-// server (not more often, than once in 15 seconds). After 10 minutes of
-// unsuccessful retries give up and deny access, so someone goes to fix
-// LDAP server.
-//
-// Like in DNS, the following condition must be always met:
-// cache_retry <= cache_refresh <= cache_expiry
-//
-// To disable LDAP cache completely, set cache_refresh, cache_retry and
-// cache_expiry to 0.
-	'cache_refresh' => 300,
-	'cache_retry' => 15,
-	'cache_expiry' => 600,
-// Uncomment line below to work around ActiveDirectory+PHP problem to perform
-// search right in the base DN (not in one of OUs/CNs below).
-#	'options' => array (LDAP_OPT_PROTOCOL_VERSION => 3, LDAP_OPT_REFERRALS => 0),
-);
+#\$LDAP_options = array
+#(
+#	'server' => 'localhost',
+#	'domain' => 'example.com',
+#	'search_attr' => '',
+#	'search_dn' => '',
+#	'displayname_attrs' => '',
+#	'options' => array (LDAP_OPT_PROTOCOL_VERSION => 3),
+#);
 
 
 ENDOFTEXT
