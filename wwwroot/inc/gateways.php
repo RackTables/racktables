00073491dda493445c327e11e35eeb52685b78fc
Denis Ovsienko <infrastation@yandex.ru>
2011-09-19 09:38:08 +0000

    r4798 detectDeviceBreed(): recognize JunOS 10 on a router

--- wwwroot/inc/gateways.php
+++ wwwroot/inc/gateways.php
@@ -331,6 +331,7 @@ function detectDeviceBreed ($object_id)
 		1369 => 'vrp55', // VRP versions 5.5 and 5.7 seem to be compatible
 		1363 => 'fdry5',
 		1367 => 'jun10',
+		1597 => 'jun10',
 		1594 => 'ftos8',
 	);
 	foreach (getAttrValues ($object_id) as $record)
