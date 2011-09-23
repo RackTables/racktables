1c0a61e28c421d33aaa436e016f91178e1943b5c
Denis Ovsienko <infrastation@yandex.ru>
2011-09-23 09:26:04 +0000

    r4801 getRunning8021QConfig(): empty "portconfig" is not an error, but empty "portdata" is

--- wwwroot/inc/remote.php
+++ wwwroot/inc/remote.php
@@ -236,7 +236,7 @@ function getRunning8021QConfig ($object_id)
 	$ret = queryDevice ($object_id, 'get8021q');
 	// Once there is no default VLAN in the parsed data, it means
 	// something else was parsed instead of config text.
-	if (!in_array (VLAN_DFL_ID, $ret['vlanlist']) || empty ($ret['portconfig']))
+	if (!in_array (VLAN_DFL_ID, $ret['vlanlist']) || empty ($ret['portdata']))
 		throw new RTGatewayError ('communication with device failed');
 	return $ret;
 }
