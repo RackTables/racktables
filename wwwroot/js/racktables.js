53b3d36b76ec876b2ed23899262e11927a3a3e8d
Aaron Dummer <aaron@dummer.info>
2011-12-31 02:19:16 +0000

    r4876 added an 'Uncheck all' button to the rackspace allocation page (#490)

--- wwwroot/js/racktables.js
+++ wwwroot/js/racktables.js
@@ -3,6 +3,8 @@
 // Used for (un)checking an entire row of rackspace atoms
 function toggleRowOfAtoms (rackId, rowId) {
 	var checkboxId;
+	var toSet;
+	toSet = null;
 	for (var i=0; i<=2; i++) {
 		checkboxId = "atom_" + rackId + "_" + rowId + "_" + i;
 
@@ -10,17 +12,18 @@ function toggleRowOfAtoms (rackId, rowId) {
 		if (document.getElementById(checkboxId).disabled == true) continue;
 
 		// Box isn't disabled, toggle it
-		if (document.getElementById(checkboxId).checked == false) {
-			document.getElementById(checkboxId).checked = true;
-		} else {
-			document.getElementById(checkboxId).checked = false;
+		if (toSet == null) {
+			toSet = !document.getElementById(checkboxId).checked;
 		}
+		document.getElementById(checkboxId).checked = toSet;
 	}
 }
 
 // Used for (un)checking an entire column of rackspace atoms
 function toggleColumnOfAtoms (rackId, columnId, numRows) {
 	var checkboxId;
+	var toSet;
+	toSet = null;
 	for (var i=1; i<numRows+1; i++) {
 		checkboxId = "atom_" + rackId + "_" + i + "_" + columnId;
 
@@ -28,12 +31,25 @@ function toggleColumnOfAtoms (rackId, columnId, numRows) {
 		if (document.getElementById(checkboxId).disabled == true) continue;
 
 		// Box isn't disabled, toggle it
-		if (document.getElementById(checkboxId).checked == false) {
-			document.getElementById(checkboxId).checked = true;
-		} else {
-			document.getElementById(checkboxId).checked = false;
+		if (toSet == null) {
+			toSet = !document.getElementById(checkboxId).checked;
 		}
+		document.getElementById(checkboxId).checked = toSet;
 	}
+}
+
+function uncheckAll () {
+    // Get all of the inputs that are in this form
+    var elements = document.getElementsByTagName("input");
+
+    for (var i = 0; i < elements.length; i++) {
+	// Abort if the box is disabled
+	if (elements[i].disabled == true) continue;
+
+	// If it's a checkbox, uncheck it
+        if (elements[i].type == "checkbox")
+            elements[i].checked = false;
+    }
 }
 
 // used by 802.1Q sync form
@@ -60,36 +76,6 @@ function checkColumnOfRadios (prefix, numRows, suffix)
 				elemId = prefix + i + '_asis';
 			}
 		document.getElementById(elemId).checked = true;
-	}
-}
-
-function blockToggleRowOfAtoms (rackId, rowId) {
-	var checkboxId;
-	var toSet;
-	toSet = null;
-	for (var i=0; i<=2; i++) {
-		checkboxId = "atom_" + rackId + "_" + rowId + "_" + i;
-		// Abort if the box is disabled
-		if (document.getElementById(checkboxId).disabled == true) continue;
-		if (toSet == null) {
-			toSet = !document.getElementById(checkboxId).checked;
-		}
-		document.getElementById(checkboxId).checked = toSet;
-	}
-}
-
-function blockToggleColumnOfAtoms (rackId, columnId, numRows) {
-	var checkboxId;
-	var toSet;
-	toSet = null;
-	for (var i=1; i<numRows+1; i++) {
-		checkboxId = "atom_" + rackId + "_" + i + "_" + columnId;
-		// Abort if the box is disabled
-		if (document.getElementById(checkboxId).disabled == true) continue;
-		if (toSet == null) {
-			toSet = !document.getElementById(checkboxId).checked;
-		}
-		document.getElementById(checkboxId).checked = toSet;
 	}
 }
 
