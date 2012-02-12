0af976747fe196322d8c141a8cfbde8c750a6cab
Denis Ovsienko <infrastation@yandex.ru>
2012-02-12 10:12:11 +0000

    r4958 introduce 10GBase-T OIF type

--- wwwroot/inc/upgrade.php
+++ wwwroot/inc/upgrade.php
@@ -1231,6 +1231,7 @@ CREATE TABLE `CactiGraph` (
 				$query[] = "UPDATE Port SET type = ${stays} WHERE type IN(${csv})";
 				$query[] = "DELETE FROM PortInterfaceCompat WHERE oif_id IN(${csv})";
 			}
+			$query[] = "INSERT INTO `PortCompat` (`type1`, `type2`) VALUES (1642,1642)";
 			$query = array_merge ($query, reloadDictionary ());
 			$query[] = "UPDATE Config SET varvalue = '0.19.11' WHERE varname = 'DB_VERSION'";
 			break;
