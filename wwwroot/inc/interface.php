aba58620d7e41d71889d6a6a7b1c81cd2dd45bc2
Denis Ovsienko <infrastation@yandex.ru>
2012-04-07 10:03:55 +0000

    r5032 hardware warranty report by Ernest Shaffer
    hwExpireReport(): new function
    scanAttrRelativeDays(): new function

--- wwwroot/inc/interface.php
+++ wwwroot/inc/interface.php
@@ -9383,4 +9383,47 @@ function renderObjectCactiGraphs ($object_id)
 	finishPortlet ();
 }
 
+function hwExpireReport ()
+{
+	global $nextorder;
+	$breakdown = array
+	(
+		array ('from' => -365, 'to' => 0, 'class' => 'has_problems_', 'title' => 'HW warranty has expired within last year'),
+		array ('from' => 0, 'to' => 30, 'class' => 'row_', 'title' => 'HW warranty expires within 30 days'),
+		array ('from' => 30, 'to' => 60, 'class' => 'row_', 'title' => 'HW warranty expires within 60 days'),
+		array ('from' => 60, 'to' => 90, 'class' => 'row_', 'title' => 'HW warranty expires within 90 days'),
+	);
+	foreach ($breakdown as $section)
+	{
+		$count = 1;
+		$order = 'odd';
+		$result = scanAttrRelativeDays (22, '%m/%d/%Y', $section['from'], $section['to']);
+
+		startPortlet ($section['title']);
+		echo "<table align=center width=60% border=0 cellpadding=5 cellspacing=0 align=center class=cooltable><tr valign=top>";
+		echo "<th align=center>Count</th>";
+		echo "<th align=center>Name</th>";
+		echo "<th align=center>Asset Tag</th>";
+		echo "<th align=center>Date Warranty <br> Expires</th></tr>\n";
+
+		if (! count ($result))
+			echo '<tr><td colspan=4>(none)</td></tr>';
+		else
+			foreach ($result as $row)
+			{
+				$object = spotEntity ('object', $row['object_id']);
+				echo '<tr class=' . $section['class'] . $order . ' valign=top>';
+				echo "<td>${count}</td>";
+				echo '<td>' . mkA ($object['dname'], 'object', $object['id']) . '</td>';
+				echo "<td>${object['asset_no']}</td>";
+				echo "<td>${row['string_value']}</td>";
+				echo "</tr>\n";
+				$order = $nextorder[$order];
+				$count++;
+			}
+		echo "</table>\n";
+		finishPortlet ();
+	}
+}
+
 ?>
