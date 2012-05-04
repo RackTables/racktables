0ec44a911ae4e497638399a8754968ceb758d116
Alexey Andriyanov <alan@al-an.info>
2012-05-04 12:41:07 +0000

    r5077 update: more config variables can be overriden per user

--- wwwroot/inc/ophandlers.php
+++ wwwroot/inc/ophandlers.php
@@ -1367,6 +1367,8 @@ function resetUIConfig()
 	setConfigVar ('CACTI_USERNAME', '');
 	setConfigVar ('CACTI_USERPASS', '');
 	setConfigVar ('VIRTUAL_OBJ_LISTSRC', '1504,1505,1506,1507');
+	setConfigVar ('DATETIME_ZONE', 'UTC');
+	setConfigVar ('DATETIME_FORMAT', 'm/d/Y');
 	return showFuncMessage (__FUNCTION__, 'OK');
 }
 
