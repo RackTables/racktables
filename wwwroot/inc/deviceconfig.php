4d6dd1d46d4dd0a4125fdf45103a7eee56941555
Alexey Andriyanov <alan@al-an.info>
2012-09-14 17:55:33 +0400

    revert last commit (f8c8b7)
    	The root of this issue is that Nexus 7000 switches have different
    	port config syntax according to the type of linear card (F1/F2).
    
    	The issue can not be fixed by introducing new breed, so I'm giving
    	up and leaving this thing as is.

--- wwwroot/inc/deviceconfig.php
+++ wwwroot/inc/deviceconfig.php
@@ -1002,9 +1002,7 @@ function nxos4PickSwitchportCommand (&$work, $line)
 		$work['portconfig'][$port_name][] = array ('type' => 'line-header', 'line' => $line);
 		// fill in defaults
 		if (!array_key_exists ('mode', $work['current']))
-			$work['current']['mode'] = array_key_exists ('switchport', $work['current'])
-				? 'access'
-				: 'SKIP';
+			$work['current']['mode'] = 'access';
 		// save work, if it makes sense
 		switch ($work['current']['mode'])
 		{
@@ -1059,9 +1057,6 @@ function nxos4PickSwitchportCommand (&$work, $line)
 	$line_class = 'line-8021q';
 	switch (TRUE)
 	{
-	case (preg_match ('@^  switchport$@', $line)):
-		$work['current']['switchport'] = TRUE;
-		break;
 	case (preg_match ('@^  switchport mode (.+)$@', $line, $matches)):
 		$work['current']['mode'] = $matches[1];
 		break;
