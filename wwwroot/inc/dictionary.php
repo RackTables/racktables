c403a7f09edade3d774ea7ee6939f5328a83f849
Alexey Andriyanov <alan@al-an.info>
2013-01-16 21:01:24 +0400

    added Catalyst 2960-SF series dictionary items

--- wwwroot/inc/dictionary.php
+++ wwwroot/inc/dictionary.php
@@ -1985,6 +1985,13 @@ $dictionary = array
 	1899 => array ('chapter_id' => 12, 'dict_value' => 'Cisco%GPASS%Catalyst 2960-8TC-S'),
 	1900 => array ('chapter_id' => 12, 'dict_value' => 'Cisco%GPASS%Catalyst 2960PD-8TT-L'),
 	1901 => array ('chapter_id' => 14, 'dict_value' => 'Cisco IOS 15.0'),
+	1902 => array ('chapter_id' => 12, 'dict_value' => 'Cisco%GPASS%Catalyst 2960S-F48FPS-L'),
+	1903 => array ('chapter_id' => 12, 'dict_value' => 'Cisco%GPASS%Catalyst 2960S-F48LPS-L'),
+	1904 => array ('chapter_id' => 12, 'dict_value' => 'Cisco%GPASS%Catalyst 2960S-F24PS-L'),
+	1905 => array ('chapter_id' => 12, 'dict_value' => 'Cisco%GPASS%Catalyst 2960S-F48TS-L'),
+	1906 => array ('chapter_id' => 12, 'dict_value' => 'Cisco%GPASS%Catalyst 2960S-F24TS-L'),
+	1907 => array ('chapter_id' => 12, 'dict_value' => 'Cisco%GPASS%Catalyst 2960S-F48TS-S'),
+	1908 => array ('chapter_id' => 12, 'dict_value' => 'Cisco%GPASS%Catalyst 2960S-F24TS-S'),
 
 # Any new "default" dictionary records must go above this line (i.e., with
 # dict_key code less, than 50000). This is necessary to keep AUTO_INCREMENT
