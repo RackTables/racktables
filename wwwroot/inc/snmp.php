a1fc412e9eab64cc588d6425c21fca0452bb23e0
Aaron Dummer <aaron@dummer.info>
2013-04-06 09:45:45 -0700

    add SNMP support for Dell PowerConnect 2824 (#815)

--- wwwroot/inc/snmp.php
+++ wwwroot/inc/snmp.php
@@ -1323,6 +1323,15 @@ $iftable_processors['dell-g21-to-g24-combo-1000SFP'] = array
 	'try_next_proc' => TRUE,
 );
 
+$iftable_processors['dell-g23-to-g24-combo-1000SFP'] = array
+(
+	'pattern' => '@^g(23|24)$@',
+	'replacement' => 'g\\1',
+	'dict_key' => '4-1077',
+	'label' => '\\1',
+	'try_next_proc' => TRUE,
+);
+
 $iftable_processors['dell-any-1000SFP'] = array
 (
 	'pattern' => '@^g(\d+)$@',
@@ -2545,6 +2554,13 @@ $known_switches = array // key is system OID w/o "enterprises" prefix
 		'processors' => array ('generic-g45-to-g48-combo-1000SFP', 'generic-g-any-1000T'),
 		'ifDescrOID' => 'ifName',
 	),
+	'674.10895.3028' => array
+	(
+		'dict_key' => 1063,
+		'text' => 'PowerConnect 2824: 22 RJ-45/10-100-1000T(X) + 2 combo ports',
+		'processors' => array ('dell-g23-to-g24-combo-1000SFP', 'generic-g-any-1000T'),
+		'ifDescrOID' => 'ifName',
+	),
 	'10977.11825.11833.97.25451.12800.100.4.4' => array
 	(
 		'dict_key' => 577,
@@ -2922,7 +2938,7 @@ function doSwitchSNMPmining ($objectInfo, $device)
 	case preg_match ('/^674\.10895\.4/', $sysObjectID): // Dell PowerConnect
 	case preg_match ('/^674\.10895\.300(3|4|7|9)/', $sysObjectID):
 	case preg_match ('/^674\.10895\.301(0|4|7|9)/', $sysObjectID):
-	case preg_match ('/^674\.10895\.302(0|1)/', $sysObjectID):
+	case preg_match ('/^674\.10895\.302(0|1|8)/', $sysObjectID):
 	case preg_match ('/^3955\.6\.1\.2048\.1/', $sysObjectID): // Linksys
 	case preg_match ('/^3955\.6\.5024/', $sysObjectID):
 	case preg_match ('/^11863\.6\.10\.58/', $sysObjectID): // TPLink
