3dde411d404b83ed6a776c01c2e4d6d288065046
Matthew Kelch <kelchm@gmail.com>
2013-09-12 16:52:48 -0400

    Move bulkselector.js contents to existing racktables.js

--- wwwroot/js/racktables.js
+++ wwwroot/js/racktables.js
@@ -97,3 +97,15 @@ $(document).ready (function (e) {
 		return confirm ("Are you sure?");
 	})
 });
+
+// Used for bulk selection and deselection of object rackspace checkboxes
+$(function () {
+    $("#selectableRack tbody").selectable({
+        filter: 'td.atom',
+        stop: function () {
+            $(".ui-selected input:enabled", this).each(function () {
+                this.checked = !this.checked
+            });
+        }
+    });
+});
