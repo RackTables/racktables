cca3025a8214e3d2b4da2354e66e389109a01f5a
Sebastian Mogilowski <smogilowski@oprago.com>
2013-12-16 21:04:45 +0400

    SNMP: list HP J9085A (2610-24)

--- wwwroot/inc/snmp.php
+++ wwwroot/inc/snmp.php
@@ -2449,6 +2449,12 @@ $known_switches = array // key is system OID w/o "enterprises" prefix
 		'text' => 'J9050A: 48 RJ-45/10-100-1000T',
 		'processors' => array ('procurve-chassis-1000T'),
 	),
+	'11.2.3.7.11.76' => array
+	(
+		'dict_key' => 860,
+		'text' => 'J9085A: 24 RJ-45/10-100TX + 2 1000T + 2 SFP-1000',
+		'processors' => array ('procurve-25-to-26-1000T', 'procurve-27-to-28-1000SFP', 'procurve-chassis-100TX'),
+	),
 	'11.2.3.7.11.78' => array
 	(
 		'dict_key' => 861,
