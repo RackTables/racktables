2a7d996429665a27271b0274f40baeb46b9aaa5e
Aaron Dummer <aaron@dummer.info>
2013-12-26 09:35:59 -0800

    LinkTriggerTest: setUp and tearDown the fixture once, instead of before and after each test

--- tests/LinkTriggerTest.php
+++ tests/LinkTriggerTest.php
@@ -6,18 +6,18 @@
 //   - porta is compatibile with portb
 class LinkTriggerTest extends PHPUnit_Framework_TestCase
 {
-	protected $autoports_config_var = NULL;
-	protected $object_id = NULL;
-	protected $porta = NULL;
-	protected $portb = NULL;
-	protected $portc = NULL;
-	protected $portc_type = NULL;
+	protected static $autoports_config_var;
+	protected static $object_id;
+	protected static $porta;
+	protected static $portb;
+	protected static $portc;
+	protected static $portc_type;
 
-	public function setUp ()
+	public static function setUpBeforeClass ()
 	{
 		// make sure AUTOPORTS_CONFIG is empty
-		$this->autoports_config_var = getConfigVar ('AUTOPORTS_CONFIG'); 
-		if ($this->autoports_config_var != '')
+		self::$autoports_config_var = getConfigVar ('AUTOPORTS_CONFIG'); 
+		if (self::$autoports_config_var != '')
 			setConfigVar ('AUTOPORTS_CONFIG', '');
 
 		// find a port type that is incompatible with 1000Base-T
@@ -25,25 +25,35 @@ class LinkTriggerTest extends PHPUnit_Framework_TestCase
 		(
 			'SELECT type1 FROM PortCompat WHERE type1 != 24 AND type2 != 24 LIMIT 1'
 		);
-		$this->portc_type = $result->fetchColumn ();
+		self::$portc_type = $result->fetchColumn ();
 
 		// add sample data
 		//   - set port a & b's type to 1000Base-T
 		//   - set port c's type to the incompatible one
-		$this->object_id = commitAddObject ('unit test object', NULL, 4, NULL);
-		$this->porta = commitAddPort ($this->object_id, 'test porta', '1-24', NULL, NULL);
-		$this->portb = commitAddPort ($this->object_id, 'test portb', '1-24', NULL, NULL);
-		$this->portc = commitAddPort ($this->object_id, 'test portc', $this->portc_type, NULL, NULL);
+		self::$object_id = commitAddObject ('unit test object', NULL, 4, NULL);
+		self::$porta = commitAddPort (self::$object_id, 'test porta', '1-24', NULL, NULL);
+		self::$portb = commitAddPort (self::$object_id, 'test portb', '1-24', NULL, NULL);
+		self::$portc = commitAddPort (self::$object_id, 'test portc', self::$portc_type, NULL, NULL);
+	}
+
+	public static function tearDownAfterClass ()
+	{
+		// restore AUTOPORTS_CONFIG to original setting
+		if (self::$autoports_config_var != '')
+			setConfigVar ('AUTOPORTS_CONFIG', self::$autoports_config_var);
+
+		// remove sample data
+		commitDeleteObject (self::$object_id);
 	}
 
 	public function tearDown ()
 	{
-		// restore AUTOPORTS_CONFIG to original setting
-		if ($this->autoports_config_var != '')
-			setConfigVar ('AUTOPORTS_CONFIG', $this->autoports_config_var);
-
-		// remove sample data
-		commitDeleteObject ($this->object_id);
+		// delete any links created during the test
+		usePreparedExecuteBlade
+		(
+			'DELETE FROM Link WHERE porta IN (?,?,?) OR portb IN (?,?,?)',
+			array (self::$porta, self::$portb, self::$portc, self::$porta, self::$portb, self::$portc)
+		);
 	}
 
 	/**
@@ -53,7 +63,7 @@ class LinkTriggerTest extends PHPUnit_Framework_TestCase
 	{
 		usePreparedInsertBlade (
 			'Link',
-			array ('porta' => $this->porta, 'portb' => $this->porta)
+			array ('porta' => self::$porta, 'portb' => self::$porta)
 		);
 	}
 
@@ -64,12 +74,12 @@ class LinkTriggerTest extends PHPUnit_Framework_TestCase
 	{
 		usePreparedInsertBlade (
 			'Link',
-			array ('porta' => $this->porta, 'portb' => $this->portb)
+			array ('porta' => self::$porta, 'portb' => self::$portb)
 		);
 		usePreparedUpdateBlade (
 			'Link',
-			array ('porta' => $this->porta, 'portb' => $this->porta),
-			array ('porta' => $this->porta, 'portb' => $this->portb)
+			array ('porta' => self::$porta, 'portb' => self::$porta),
+			array ('porta' => self::$porta, 'portb' => self::$portb)
 		);
 	}
 
@@ -77,12 +87,12 @@ class LinkTriggerTest extends PHPUnit_Framework_TestCase
 	{
 		usePreparedInsertBlade (
 			'Link',
-			array ('porta' => $this->portb, 'portb' => $this->porta)
+			array ('porta' => self::$portb, 'portb' => self::$porta)
 		);
 		$result = usePreparedSelectBlade
 		(
 			'SELECT COUNT(*) FROM Link WHERE porta=? AND portb=?',
-			array ($this->porta, $this->portb)
+			array (self::$porta, self::$portb)
 		);
 		$this->assertEquals ($result->fetchColumn (), 1);
 	}
@@ -91,17 +101,17 @@ class LinkTriggerTest extends PHPUnit_Framework_TestCase
 	{
 		usePreparedInsertBlade (
 			'Link',
-			array ('porta' => $this->porta, 'portb' => $this->portb)
+			array ('porta' => self::$porta, 'portb' => self::$portb)
 		);
 		usePreparedUpdateBlade (
 			'Link',
-			array ('porta' => $this->portb, 'portb' => $this->porta),
-			array ('porta' => $this->porta, 'portb' => $this->portb)
+			array ('porta' => self::$portb, 'portb' => self::$porta),
+			array ('porta' => self::$porta, 'portb' => self::$portb)
 		);
 		$result = usePreparedSelectBlade
 		(
 			'SELECT COUNT(*) FROM Link WHERE porta=? AND portb=?',
-			array ($this->porta, $this->portb)
+			array (self::$porta, self::$portb)
 		);
 		$this->assertEquals ($result->fetchColumn (), 1);
 	}
@@ -113,7 +123,7 @@ class LinkTriggerTest extends PHPUnit_Framework_TestCase
 	{
 		usePreparedInsertBlade (
 			'Link',
-			array ('porta' => $this->porta, 'portb' => $this->portc)
+			array ('porta' => self::$porta, 'portb' => self::$portc)
 		);
 	}
 
@@ -124,12 +134,12 @@ class LinkTriggerTest extends PHPUnit_Framework_TestCase
 	{
 		usePreparedInsertBlade (
 			'Link',
-			array ('porta' => $this->porta, 'portb' => $this->portb)
+			array ('porta' => self::$porta, 'portb' => self::$portb)
 		);
 		usePreparedUpdateBlade (
 			'Link',
-			array ('porta' => $this->porta, 'portb' => $this->portc),
-			array ('porta' => $this->porta, 'portb' => $this->portb)
+			array ('porta' => self::$porta, 'portb' => self::$portc),
+			array ('porta' => self::$porta, 'portb' => self::$portb)
 		);
 	}
 }
