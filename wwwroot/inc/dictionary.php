8db8136867edcec56b51377da94f822448b955a9
Alexey Andriyanov <alan@al-an.info>
2015-01-13 13:47:10 +0300

    dict: remove duplicate item HP ProCurve 1910-24G

--- wwwroot/inc/dictionary.php
+++ wwwroot/inc/dictionary.php
@@ -2271,7 +2271,6 @@ $dictionary = array
 	2379 => array ('chapter_id' => 12, 'dict_value' => 'Cisco%GPASS%SG300-52MP'),
 	2380 => array ('chapter_id' => 13, 'dict_value' => 'SUSE Enterprise%GSKIP%SLES12'),
 	2381 => array ('chapter_id' => 12, 'dict_value' => 'MikroTik%GPASS%CRS226-24G-2S+RM'),
-	2382 => array ('chapter_id' => 12, 'dict_value' => 'HP ProCurve%GPASS%1910-24G'),
 
 
 # Any new "default" dictionary records must go above this line (i.e., with
