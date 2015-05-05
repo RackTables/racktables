fb27a2b10421774dedac48ba93b4ebf9931f8f99
Ray Robertson <ray.robertson@eckoh.com>
2015-05-05 16:46:35 +0100

    Update dictionary.php
    Added Dell 13G servers and an HP model we have come across.

--- wwwroot/inc/dictionary.php
+++ wwwroot/inc/dictionary.php
@@ -2273,6 +2273,13 @@ $dictionary = array
 	2381 => array ('chapter_id' => 12, 'dict_value' => 'MikroTik%GPASS%CRS226-24G-2S+RM'),
 	2382 => array ('chapter_id' => 12, 'dict_value' => 'Huawei%GPASS%CE5850-48T4S2Q-HI'),
 	2383 => array ('chapter_id' => 12, 'dict_value' => '[[Cisco%GPASS%Nexus 3132Q | http://www.cisco.com/c/en/us/products/switches/nexus-3132q-switch/index.html]]'),
+	2384 => array ('chapter_id' => 11, 'dict_value' => 'HP ProLiant%GPASS%DL120'),
+	2385 => array ('chapter_id' => 11, 'dict_value' => 'Dell PowerEdge (blade)%GPASS%M630'),
+	2386 => array ('chapter_id' => 11, 'dict_value' => 'Dell PowerEdge%GPASS%R430'),
+	2387 => array ('chapter_id' => 11, 'dict_value' => 'Dell PowerEdge%GPASS%R530'),
+	2388 => array ('chapter_id' => 11, 'dict_value' => 'Dell PowerEdge%GPASS%R630'),
+	2389 => array ('chapter_id' => 11, 'dict_value' => 'Dell PowerEdge%GPASS%R730'),
+	2390 => array ('chapter_id' => 11, 'dict_value' => 'Dell PowerEdge%GPASS%R730xd'),
 
 
 # Any new "default" dictionary records must go above this line (i.e., with
