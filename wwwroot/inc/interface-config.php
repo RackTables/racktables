004c2fd261df3093209d6d2399bd7e4203d98469
Denis Ovsienko <denis@ovsienko.info>
2016-01-07 20:30:20 +0000

    add missing licensing headers

--- wwwroot/inc/interface-config.php
+++ wwwroot/inc/interface-config.php
@@ -1,5 +1,9 @@
 <?php
 
+# This file is a part of RackTables, a datacenter and server room management
+# framework. See accompanying file "COPYING" for the full copyright and
+# licensing information.
+
 // Find direct sub-pages and dump as a list.
 // FIXME: assume all config kids to have static titles at the moment,
 // but use some proper abstract function later.
