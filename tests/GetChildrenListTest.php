2d2bf001c84901b26c3dc4190547848bbcc2d411
Denis Ovsienko <denis@ovsienko.info>
2016-02-09 23:44:55 +0000

    tests: refine GetChildrenListTest
    Change the class to keep the complete list of the IDs being added on
    setup and to delete the rows one by one in the reverse order on
    teardown. This way the teardown method is more compact and the foreign
    key constraints remain satisfied all the time.
    
    Use commitDeleteObject() to delete the objects and locations as this
    covers any associated rows from related tables. In particular, before
    this change ObjectHistory would contain stray rows after each test run.

--- tests/GetChildrenListTest.php
+++ tests/GetChildrenListTest.php
@@ -5,44 +5,47 @@ class GetChildrenListTest extends PHPUnit_Framework_TestCase
 {
 	protected static $num_children = 10;
 	protected static $objtype_id;
-	protected static $first_location_id, $last_location_id;
-	protected static $first_object_id, $last_object_id;
-	protected static $first_tag_id, $last_tag_id;
+	protected static $locations = array();
+	protected static $objects = array();
+	protected static $links = array();
+	protected static $tags = array();
 
 	public static function setUpBeforeClass ()
 	{
 		// add sample locations
-		self::$first_location_id = $parent_location_id = commitAddObject ('unit test location 0', NULL, 1562, NULL);
+		self::$locations[] = $parent_location_id = commitAddObject ('unit test location 0', NULL, 1562, NULL);
 		for ($i=1; $i<=self::$num_children; $i++)
 		{
 			$child_location_id = commitAddObject ("unit test location ${i}", NULL, 1562, NULL);
+			self::$locations[] = $child_location_id;
 			commitLinkEntities ('location', $parent_location_id, 'location', $child_location_id);
+			self::$links[] = lastInsertID();
 			$parent_location_id = $child_location_id;
 		}
-		self::$last_location_id = $parent_location_id;
 
 		// add sample objects
 		usePreparedInsertBlade ('Dictionary', array ('chapter_id' => 1, 'dict_value' => 'unit test object type'));
 		self::$objtype_id = lastInsertID ();
 		commitSupplementOPC (self::$objtype_id, self::$objtype_id);
-		self::$first_object_id = $parent_object_id = commitAddObject ('unit test object 0', NULL, self::$objtype_id, NULL);
+		self::$objects[] = $parent_object_id = commitAddObject ('unit test object 0', NULL, self::$objtype_id, NULL);
 		for ($i=1; $i<=self::$num_children; $i++)
 		{
 			$child_object_id = commitAddObject ("unit test object ${i}", NULL, self::$objtype_id, NULL);
+			self::$objects[] = $child_object_id;
 			commitLinkEntities ('object', $parent_object_id, 'object', $child_object_id);
+			self::$links[] = lastInsertID();
 			$parent_object_id = $child_object_id;
 		}
-		self::$last_object_id = $parent_object_id;
 
 		// add sample tags
 		usePreparedInsertBlade ('TagTree', array ('tag' => 'unit test tag 0'));
-		self::$first_tag_id = $parent_tag_id = lastInsertID ();
+		self::$tags[] = $parent_tag_id = lastInsertID ();
 		for ($i=1; $i<=self::$num_children; $i++)
 		{
 			usePreparedInsertBlade ('TagTree', array ('parent_id' => $parent_tag_id, 'tag' => "unit test tag ${i}"));
 			$parent_tag_id = lastInsertID ();
+			self::$tags[] = $parent_tag_id;
 		}
-		self::$last_tag_id = $parent_tag_id;
 		// Refresh the structured version of the table so that getTagDescendents() works as expected.
 		global $taglist;
 		$taglist = addTraceToNodes (getTagList());
@@ -50,68 +53,33 @@ class GetChildrenListTest extends PHPUnit_Framework_TestCase
 
 	public static function tearDownAfterClass ()
 	{
-		usePreparedExecuteBlade ('SET foreign_key_checks=0');
-
-		// remove sample locations
-		usePreparedExecuteBlade
-		(
-			'DELETE FROM Object WHERE id BETWEEN ? AND ?',
-			array (self::$first_location_id, self::$last_location_id)
-		);
-		usePreparedExecuteBlade
-		(
-			"DELETE FROM EntityLink WHERE parent_entity_type='location' AND child_entity_type='location' " .
-			'AND ((parent_entity_id BETWEEN ? AND ?) OR (child_entity_id BETWEEN ? AND ?))',
-			array
-			(
-				self::$first_location_id, self::$last_location_id,
-				self::$first_location_id, self::$last_location_id
-			)
-		);
-
-		// remove sample objects
-		usePreparedExecuteBlade
-		(
-			'DELETE FROM Object WHERE id BETWEEN ? AND ?',
-			array (self::$first_object_id, self::$last_object_id)
-		);
-		usePreparedExecuteBlade
-		(
-			"DELETE FROM EntityLink WHERE parent_entity_type='object' AND child_entity_type='object' " .
-			'AND ((parent_entity_id BETWEEN ? AND ?) OR (child_entity_id BETWEEN ? AND ?))',
-			array
-			(
-				self::$first_object_id, self::$last_object_id,
-				self::$first_object_id, self::$last_object_id
-			)
-		);
+		foreach (array_reverse (self::$links) as $each)
+			commitUnlinkEntitiesByLinkID ($each);
+		foreach (array_reverse (self::$objects) as $each)
+			commitDeleteObject ($each);
+		foreach (array_reverse (self::$locations) as $each)
+			commitDeleteObject ($each);
+		foreach (array_reverse (self::$tags) as $each)
+			usePreparedDeleteBlade ('TagTree', array ('id' => $each));
 		commitReduceOPC (self::$objtype_id, self::$objtype_id);
 		usePreparedDeleteBlade ('Dictionary', array ('dict_key' => self::$objtype_id));
-
-		// remove sample tags
-		usePreparedExecuteBlade
-		(
-			'DELETE FROM TagTree WHERE id BETWEEN ? AND ?',
-			array (self::$first_tag_id, self::$last_tag_id)
-		);
-		usePreparedExecuteBlade ('SET foreign_key_checks=1');
 	}
 
 	public function testGetLocationChildrenList ()
 	{
-		$children = getLocationChildrenList (self::$first_location_id);
+		$children = getLocationChildrenList (array_first (self::$locations));
 		$this->assertCount (self::$num_children, $children);
 	}
 
 	public function testGetObjectChildrenList ()
 	{
-		$children = getObjectContentsList (self::$first_object_id);
+		$children = getObjectContentsList (array_first (self::$objects));
 		$this->assertCount (self::$num_children, $children);
 	}
 
 	public function testGetTagChildrenList ()
 	{
-		$children = getTagDescendents (self::$first_tag_id);
+		$children = getTagDescendents (array_first (self::$tags));
 		$this->assertCount (self::$num_children, $children);
 	}
 }
