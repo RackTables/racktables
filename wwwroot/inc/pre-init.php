0c53bc48808489b4ee1f7cda725aebe121713385
Denis Ovsienko <denis@ovsienko.info>
2016-06-14 16:19:12 +0100

    work around MySQL 5.7 SQL mode (#1547, #1635)
    Use a temporary workaround to make RackTables work with MySQL 5.7
    before proper bugfixes have been made. Commit 8ea0abc was the first
    step in that direction and its commit message explains the problem.

--- wwwroot/inc/pre-init.php
+++ wwwroot/inc/pre-init.php
@@ -58,7 +58,11 @@ function connectDB()
 	$drvoptions = array
 	(
 		PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-		PDO::MYSQL_ATTR_INIT_COMMAND => 'set names "utf8"',
+		// Cancel two specific SQL mode options that RackTables has been non-compliant
+		// with but which used to be off by default until MySQL 5.7. As soon as
+		// respective SQL queries and table columns become compliant with those options
+		// stop changing @@SQL_MODE but still keep SET NAMES in place.
+		PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES "utf8", @@SQL_MODE = REPLACE(REPLACE(@@SQL_MODE, "NO_ZERO_DATE", ""), "ONLY_FULL_GROUP_BY", "")',
 	);
 	if (isset ($pdo_bufsize))
 		$drvoptions[PDO::MYSQL_ATTR_MAX_BUFFER_SIZE] = $pdo_bufsize;
