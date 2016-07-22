RackTables/racktables

commit ad80d546ba9f6b75278b3bd2b2ac4c7849902575

Add getportlist, getmaclist, getportmaclist for fdry5. Use fdry5 breed for Brocade FastIron CX 648 and Brocade FastIron LS 648. Allow sshnokey to use $tolerate_remote_errors. Add $hide_warnings flag to hide the 'Connection to x.x.x.x closed by remote host.' error on exit when using ssh or sshnokey with fdry5.

wwwroot/inc/remote.php
@@ -27,6 +27,9 @@ $breedfunc = array
 	'fdry5-get8021q-readport'  => 'fdry5PickInterfaceSubcommand',
 	'fdry5-xlatepushq-main'    => 'fdry5TranslatePushQueue',
 	'fdry5-getallconf-main'    => 'fdry5SpotConfigText',
+	'fdry5-getportstatus-main' => 'foundryReadInterfaceStatus',
+	'fdry5-getmaclist-main'    => 'fdry5ReadMacList',
+	'fdry5-getportmaclist-main'=> 'fdry5ReadMacList',
 	'vrp53-getlldpstatus-main' => 'vrpReadLLDPStatus',
 	'vrp53-get8021q-main'      => 'vrp53ReadVLANConfig',
 	'vrp53-get8021q-top'       => 'vrp53ScanTopLevel',
@@ -149,6 +152,7 @@ $breed_by_swcode = array
 	2081 => 'vrp55', // Huawei VRP 5.12
 	2027 => 'vrp85', // Huawei VRP 8.5
 	1363 => 'fdry5', // IronWare 5
+	1364 => 'fdry5', // Brocade FastIron LS648
 	1367 => 'jun10', // JunOS 10, switch
 	1597 => 'jun10', // JunOS 10
 	1598 => 'jun10', // JunOS 11
@@ -167,6 +171,7 @@ $breed_by_swcode = array
 );
 
 $breed_by_hwcode = array (
+	1362 => 'fdry5', // Brocade FastIron CX648
 	//... dlink items added by the loop below
 );
 
@@ -440,8 +445,12 @@ function queryTerminal ($object_id, $commands, $tolerate_remote_errors = TRUE)
 			break;
 		case 'fdry5':
 			$protocol = 'netcat'; // default is netcat mode
-			$prompt = '^(Login|Username|Password): $|^\S+[>#]$'; // set the prompt in case user would like to specify telnet protocol
+			$prompt = '^(Login|Username|Password|Please Enter Password): $|^\S+[>#]$'; // set the prompt in case user would like to specify telnet protocol
 			$commands = "skip-page-display\n" . $commands;
+			# using ssh and sshnokey we'll always receive 'Connection to $ip closed by remote host.' upon exit
+			# let's hide the warnings 
+			$tolerate_remote_errors = TRUE;
+			$hide_warnings = TRUE;
 			break;
 		case 'vrp55':
 			$commands = "terminal echo-mode line\n" . $commands;
@@ -521,7 +530,7 @@ function queryTerminal ($object_id, $commands, $tolerate_remote_errors = TRUE)
 	// call gateway
 	$ret_code = callScript ($settings['protocol'], $params, $commands, $out, $errors);
 
-	if ($settings['protocol'] != 'ssh' || ! $tolerate_remote_errors)
+	if (substr($settings['protocol'],0,3) != 'ssh' || ! $tolerate_remote_errors)
 	{
 		if ($errors != '')
 			throw new RTGatewayError ("${settings['protocol']} error: " . rtrim ($errors));
@@ -530,7 +539,7 @@ function queryTerminal ($object_id, $commands, $tolerate_remote_errors = TRUE)
 	}
 	elseif ($errors != '') // ssh and tolerate and non-empty $errors
 		foreach (explode ("\n", $errors) as $line)
-			if ($line != '')
+			if ($line != '' && ! $hide_warnings)
 				showWarning ("${settings['protocol']} ${settings['hostname']}: $line");
 	return strtr($out, array("\r" => "")); // cut ^M symbols
 }

wwwroot/inc/deviceconfig.php
@@ -1346,6 +1346,15 @@ function fdry5TranslatePushQueue ($dummy_object_id, $queue, $dummy_vlan_names)
 		case 'getallconf':
 			$ret .= "show running-config\n";
 			break;
+		case 'getportstatus':
+			$ret .= "show int brief\n";
+			break;
+		case 'getmaclist':
+			$ret .= "show mac-address\n";
+			break;
+		case 'getportmaclist':
+			$ret .= "show mac-address ethernet {$cmd['arg1']}\n";
+			break;
 		default:
 			throw new InvalidArgException ('opcode', $cmd['opcode']);
 		}
@@ -2728,6 +2737,45 @@ function ros11Read8021QPorts (&$work, $line)
 	}
 }
 
+function foundryReadInterfaceStatus ($text) {
+	$result = array();
+	$state = 'headerSearch';
+	foreach (explode ("\n", $text) as $line)
+	{
+		switch ($state)
+		{
+			case 'headerSearch':
+				if (preg_match('/^Port\s+Link\s+State/', $line))
+				{
+					$link_field_borders = getColumnCoordinates($line, 'Link');
+					if (isset ($link_field_borders['from']))
+						$state = 'readPort';
+				}
+				break;
+			case 'readPort':
+				$field_list = preg_split('/\s+/', $line);
+				if (count ($field_list) < 5)
+					break;
+				list ($portname, $status_raw, $stp_state, $duplex, $speed) = $field_list;
+				if ($status_raw == 'Up' || $status_raw == 'up')
+					$status = 'up';
+				elseif ($status_raw == 'Down' || $status_raw == 'down')
+					$status = 'down';
+				else
+					$status = 'disabled';
+				$result[$portname] = array
+				(
+					'status' => $status,
+					'speed' => $speed,
+					'duplex' => $duplex,
+				);
+				break;
+		}
+	}
+
+	return $result;
+}
+
 function ciscoReadInterfaceStatus ($text)
 {
 	$result = array();
@@ -3071,6 +3119,35 @@ function maclist_sort ($a, $b)
 	if ($a['vid'] == $b['vid'])
 		return 0;
 	return ($a['vid'] < $b['vid']) ? -1 : 1;
+}
+
+function fdry5ReadMacList ($text)
+{
+	$result = array();
+	$state = 'headerSearch';
+	foreach (explode ("\n", $text) as $line)
+	{
+		switch ($state)
+		{
+			case 'headerSearch':
+				if (preg_match('/MAC-Address\s+Port\s+Type\s+Index\s+VLAN/i', $line))
+					$state = 'readPort';
+				break;
+			case 'readPort':
+				if (! preg_match ('/([a-f0-9]{4}\.[a-f0-9]{4}\.[a-f0-9]{4})\s+(\S+)\s+\S+\s+\d+\s+(\d+)$/', trim ($line), $matches))
+					break;
+				$portname = shortenIfName ($matches[2]);
+				$result[$portname][] = array
+				(
+					'mac' => $matches[1],
+					'vid' => $matches[3],
+				);
+				break;
+		}
+	}
+	foreach ($result as $portname => &$maclist)
+		usort ($maclist, 'maclist_sort');
+	return $result;
 }
 
 function ios12ReadMacList ($text)

commit c0e9968dc719148f26de5632e51fd0744f1f1e50

bugfix: use of udefined variable
queryTerminal: hide_warnings was not always defined
Fixes: ad80d546

wwwroot/inc/remote.php
@@ -431,6 +431,8 @@ function queryTerminal ($object_id, $commands, $tolerate_remote_errors = TRUE)
 	if (count ($endpoints) > 1)
 		throw new RTGatewayError ('cannot pick management address');
 
+	$hide_warnings = FALSE;
+
 	// telnet prompt and mode specification
 	switch ($breed = detectDeviceBreed ($object_id))
 	{
@@ -448,7 +450,7 @@ function queryTerminal ($object_id, $commands, $tolerate_remote_errors = TRUE)
 			$prompt = '^(Login|Username|Password|Please Enter Password): $|^\S+[>#]$'; // set the prompt in case user would like to specify telnet protocol
 			$commands = "skip-page-display\n" . $commands;
 			# using ssh and sshnokey we'll always receive 'Connection to $ip closed by remote host.' upon exit
-			# let's hide the warnings 
+			# let's hide the warnings
 			$tolerate_remote_errors = TRUE;
 			$hide_warnings = TRUE;
 			break;