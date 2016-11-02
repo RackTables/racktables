RackTables/racktables

commit 489f843c2abdf5592cdfac8ecfc43465a6b64697

eliminate a few more wildcard SELECTs
The only two instances that remain after this change stand for generic
functions that cannot be fixed in a simple way.

* SLBTriplet::getTripletsByIP()
* getUpgradeBatch()
* convertSLBTablesToBinIPs()

wwwroot/inc/upgrade.php
@@ -808,7 +808,7 @@ CREATE TABLE `Attribute_new` (
   UNIQUE KEY `name` (`name`)
 ) ENGINE=InnoDB
 ";
-			$query[] = "INSERT INTO Attribute_new SELECT * FROM Attribute";
+			$query[] = "INSERT INTO Attribute_new SELECT id, type, name FROM Attribute";
 			$query[] = "INSERT INTO Attribute_new VALUES (9999, 'string', 'base MAC address')";
 			$query[] = "DROP TABLE Attribute";
 			$query[] = "ALTER TABLE Attribute_new RENAME TO Attribute";
@@ -1064,7 +1064,7 @@ CREATE TABLE `IPv6Log` (
 			$query[] = "ALTER TABLE `EntityLink` MODIFY COLUMN `child_entity_type` ENUM('file','location','object','rack','row') NOT NULL";
 
 			// Turn rows into objects
-			$result = $dbxlink->query ('SELECT * FROM RackRow');
+			$result = $dbxlink->query ('SELECT name FROM RackRow');
 			$rows = $result->fetchAll (PDO::FETCH_ASSOC);
 			unset ($result);
 			foreach ($rows as $row)
@@ -2229,7 +2229,7 @@ CREATE TABLE `IPv4VS_new` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8
 END
 	);
-	$result = $dbxlink->query ("SELECT * FROM IPv4VS");
+	$result = $dbxlink->query ("SELECT id, vip, vport, proto, name, vsconfig, rsconfig FROM IPv4VS");
 	$rows = $result->fetchAll (PDO::FETCH_ASSOC);
 	unset ($result);
 	foreach ($rows as $row)
@@ -2254,7 +2254,7 @@ CREATE TABLE `IPv4RS_new` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8
 END
 	);
-	$result = $dbxlink->query ("SELECT * FROM IPv4RS");
+	$result = $dbxlink->query ("SELECT id, inservice, rsip, rsport, rspool_id, rsconfig FROM IPv4RS");
 	$rows = $result->fetchAll (PDO::FETCH_ASSOC);
 	unset ($result);
 	foreach ($rows as $row)

wwwroot/inc/slb.php
@@ -96,7 +96,7 @@ class SLBTriplet
 	{
 		$ret = array();
 		$result = usePreparedSelectBlade ("
-SELECT DISTINCT IPv4LB.*
+SELECT DISTINCT IPv4LB.object_id, IPv4LB.rspool_id, IPv4LB.vs_id, IPv4LB.prio, IPv4LB.vsconfig, IPv4LB.rsconfig
 FROM
 	IPv4LB INNER JOIN IPv4VS ON IPv4VS.id = IPv4LB.vs_id
 	LEFT JOIN IPv4RS USING (rspool_id)

commit 82ebf6c39c818ab243ae8ac869ae2ff55b463738

Fixed a bug introduced in 489f843c2abdf5592cdfac8ecfc43465a6b64697

wwwroot/inc/upgrade.php
@@ -1064,7 +1064,7 @@ CREATE TABLE `IPv6Log` (
 			$query[] = "ALTER TABLE `EntityLink` MODIFY COLUMN `child_entity_type` ENUM('file','location','object','rack','row') NOT NULL";
 
 			// Turn rows into objects
-			$result = $dbxlink->query ('SELECT name FROM RackRow');
+			$result = $dbxlink->query ('SELECT id, name FROM RackRow');
 			$rows = $result->fetchAll (PDO::FETCH_ASSOC);
 			unset ($result);
 			foreach ($rows as $row)