RackTables/racktables

commit 4a4f2614fe54c5726aeff6480976ca0a66207f4c

getVlanRow(): spell NULL comparison uniformly
Though there is no difference between == and === in this particular
context, use === for consistency with other similar tests so the code
can be copied and pasted without unintended effects.

wwwroot/inc/database.php
@@ -5367,7 +5367,7 @@ function getVlanRow ($vlan_ck)
 		'(SELECT group_id FROM VLANDomain WHERE id = domain_id) AS domain_group_id ' .
 		'FROM VLANDescription WHERE domain_id = ? AND vlan_id = ?';
 	$result = usePreparedSelectBlade ($query, array ($vdom_id, $vlan_id));
-	if (NULL == ($ret = $result->fetch (PDO::FETCH_ASSOC)))
+	if (NULL === $ret = $result->fetch (PDO::FETCH_ASSOC))
 		throw new EntityNotFoundException ('VLAN', $vlan_ck);
 	$ret['vlan_ck'] = $vlan_ck;
 	return $ret;

commit 4544a1ce23a855317b57982947222cd1caebc274

getVlanRow: fix NotFound error handling
Fixes: 4a4f2614

wwwroot/inc/database.php
@@ -5258,7 +5258,7 @@ function getVlanRow ($vlan_ck)
 		'(SELECT group_id FROM VLANDomain WHERE id = domain_id) AS domain_group_id ' .
 		'FROM VLANDescription WHERE domain_id = ? AND vlan_id = ?';
 	$result = usePreparedSelectBlade ($query, array ($vdom_id, $vlan_id));
-	if (NULL === $ret = $result->fetch (PDO::FETCH_ASSOC))
+	if (FALSE === $ret = $result->fetch (PDO::FETCH_ASSOC))
 		throw new EntityNotFoundException ('VLAN', $vlan_ck);
 	$ret['vlan_ck'] = $vlan_ck;
 	return $ret;