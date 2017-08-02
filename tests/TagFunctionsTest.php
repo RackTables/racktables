a433b2683235c1b7798f6b86f41848e6a62edb86
Denis Ovsienko <denis@ovsienko.info>
2017-08-02 13:37:39 +0100

    tests: improve TagFunctionsTest
    Move all read-only tests into one method to avoid duplicating the class
    fixtures. Supply an array argument to usePreparedDeleteBlade() to make
    use of SQL IN(). Assign some colours to the fixture tags and add a basic
    read-only test to cover setEntityColors().

--- tests/TagFunctionsTest.php
+++ tests/TagFunctionsTest.php
@@ -5,6 +5,17 @@ class TagFunctionsTest extends PHPUnit_Framework_TestCase
 	protected $a_obj_id, $b_obj_id, $ab_obj_id;
 	protected $a_tag_ids, $b_tag_ids;
 	const NUM_TAGS = 4;
+	const COLOR_A = 0x559900, COLOR_B = 0x112233;
+
+	protected function assertObjectColors ($object_id, $uint_colors)
+	{
+		$obj = spotEntity ('object', $object_id);
+		setEntityColors ($obj);
+		$html_colors = array_map ('HTMLColorFromDatabase', $uint_colors);
+		sort ($html_colors);
+		sort ($obj['colors']);
+		$this->assertEquals ($html_colors, $obj['colors']);
+	}
 
 	public function setUp()
 	{
@@ -15,9 +26,9 @@ class TagFunctionsTest extends PHPUnit_Framework_TestCase
 		$this->b_tag_ids = array();
 		for ($i = 0; $i < self::NUM_TAGS; $i++)
 		{
-			usePreparedInsertBlade ('TagTree', array ('tag' => "tag a${i} ${sfx}"));
+			usePreparedInsertBlade ('TagTree', array ('tag' => "tag a${i} ${sfx}", 'color' => self::COLOR_A + $i));
 			$this->a_tag_ids[] = lastInsertID();
-			usePreparedInsertBlade ('TagTree', array ('tag' => "tag b${i} ${sfx}"));
+			usePreparedInsertBlade ('TagTree', array ('tag' => "tag b${i} ${sfx}", 'color' => self::COLOR_B));
 			$this->b_tag_ids[] = lastInsertID();
 		}
 		$taglist = addTraceToNodes (getTagList());
@@ -33,34 +44,32 @@ class TagFunctionsTest extends PHPUnit_Framework_TestCase
 		commitDeleteObject ($this->a_obj_id);
 		commitDeleteObject ($this->b_obj_id);
 		commitDeleteObject ($this->ab_obj_id);
-		foreach (array_merge ($this->a_tag_ids, $this->b_tag_ids) as $tag_id)
-			usePreparedDeleteBlade ('TagTree', array ('id' => $tag_id));
+		usePreparedDeleteBlade ('TagTree', array ('id' => array_merge ($this->a_tag_ids, $this->b_tag_ids)));
 	}
 
 	/**
 	 * @group small
 	 */
-	public function testFixtures()
+	public function testReadOnly()
 	{
+		// fixtures
 		$this->assertEquals (self::NUM_TAGS, count ($this->a_tag_ids));
 		$this->assertEquals (self::NUM_TAGS, count ($this->b_tag_ids));
-	}
 
-	/**
-	 * @group small
-	 */
-	public function testLoad()
-	{
+		// loading
 		$this->assertEquals (array_values ($this->a_tag_ids), array_values (reduceSubarraysToColumn (loadEntityTags ('object', $this->a_obj_id), 'id')));
 		$this->assertEquals (array_values ($this->b_tag_ids), array_values (reduceSubarraysToColumn (loadEntityTags ('object', $this->b_obj_id), 'id')));
 		$this->assertEquals (array_values (array_merge ($this->a_tag_ids, $this->b_tag_ids)), array_values (reduceSubarraysToColumn (loadEntityTags ('object', $this->ab_obj_id), 'id')));
-	}
 
-	/**
-	 * @group small
-	 */
-	public function testTransform()
-	{
+		// transforming
 		$this->assertEquals ($this->a_tag_ids, buildTagIdsFromChain (buildTagChainFromIds ($this->a_tag_ids)));
+
+		// colors
+		$a_colors = range (self::COLOR_A, self::COLOR_A + self::NUM_TAGS - 1);
+		$this->assertObjectColors ($this->a_obj_id, $a_colors);
+		$b_colors = array (self::COLOR_B);
+		$this->assertObjectColors ($this->b_obj_id, $b_colors);
+		$ab_colors = array_merge ($a_colors, $b_colors);
+		$this->assertObjectColors ($this->ab_obj_id, $ab_colors);
 	}
 }
