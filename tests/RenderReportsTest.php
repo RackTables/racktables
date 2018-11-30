8e0ca19e727e9d6c0e6daf853eb3ff0f48609152
Denis Ovsienko <denis@ovsienko.info>
2018-11-30 21:01:09 +0000

    tests: update RenderReportsTest
    Assert that the integrity report prints the "all clear" message.
    
    Remove some dead code: the class props are no longer required since
    commit 4829189; the test condition is always true because
    renderLocalReports() does not exist since commit df8739f.

--- tests/RenderReportsTest.php
+++ tests/RenderReportsTest.php
@@ -5,18 +5,6 @@ require_once '../wwwroot/inc/navigation.php';
 // Make sure renderDepot does not throw any exceptions
 class RenderReportsTest extends RTTestCase
 {
-	public function setUp ()
-	{
-		// A minimal prop to make renderPortsReport() print something.
-		$this->object_id = commitAddObject ($this->myString ('object'), NULL, 1, NULL);
-		commitAddPort ($this->object_id, 'port 1', '1-24', 'label 1', 'aabbccffff01');
-	}
-
-	public function tearDown ()
-	{
-		commitDeleteObject ($this->object_id);
-	}
-
 	/**
 	 * @group small
 	 * @dataProvider providerReportFunctions
@@ -25,7 +13,10 @@ class RenderReportsTest extends RTTestCase
 	{
 		global $interface_requires;
 		requireExtraFiles ($interface_requires, $pageno, $tabno);
-		$this->assertNotEquals ('', getOutputOf ($funcname));
+		$output = getOutputOf ($funcname);
+		$this->assertNotEquals ('', $output);
+		if ($tabno == 'integrity')
+			$this->assertEquals ('No integrity violations found', strip_tags ($output));
 	}
 
 	public function providerReportFunctions ()
@@ -33,8 +24,7 @@ class RenderReportsTest extends RTTestCase
 		$ret = array();
 		global $tabhandler;
 		foreach ($tabhandler['reports'] as $tabno => $funcname)
-			if ($funcname != 'renderLocalReports') // That one returns an empty string by default.
-				$ret[] = array ('reports', $tabno, $funcname);
+			$ret[] = array ('reports', $tabno, $funcname);
 		return $ret;
 	}
 }
