7945663ce37b96cf2003c705f61cd3e4f118d5db
Denis Ovsienko <denis@ovsienko.info>
2019-11-19 00:11:11 +0000

    tests: refine PHPUnit presence checks
    Make express_phpunit.sh distinguish between unavailable PHPUnit (not
    installed at all or PHPUNIT_BIN pointed to a file that lacks the
    executable permission) and PHPUnit that is available, but has an
    unsupported version. Direct the error messages to stderr while at it.
    
    [skip ci]

--- tests/express_phpunit.sh
+++ tests/express_phpunit.sh
@@ -3,13 +3,18 @@
 THISDIR=`dirname "$0"`
 : ${PHPUNIT_BIN:=phpunit}
 
+if ! which "$PHPUNIT_BIN" >/dev/null; then
+	echo "ERROR: $PHPUNIT_BIN is not an executable file" >&2
+	exit 4
+fi
+
 case `"$PHPUNIT_BIN" --version` in
 	'PHPUnit 6.'*|'PHPUnit 7.'*)
 		BOOTSTRAP_FILE=bootstrap_v6v7.php
 		;;
 	*)
-		echo 'ERROR: failed to find a known version of PHPUnit'
-		"$PHPUNIT_BIN" --version
+		echo 'ERROR: unsupported PHPUnit version' >&2
+		"$PHPUNIT_BIN" --version >&2
 		exit 5
 esac
 
