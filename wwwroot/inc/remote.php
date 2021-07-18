e2308125a7be8b38eb9bdfd68f8c0eb70c383441
Denis Ovsienko <denis@ovsienko.info>
2021-07-18 22:48:17 +0100

    queryTerminal(): use working defaults for "ios15" [skip ci]
    As Mark Brugnoli-Vinten points it out in Mantis#2031, ios15 has no
    default prompt, and the default prompt of other Cisco breeds seems to
    work, so let's set ios15 defaults in the same case block.

--- wwwroot/inc/remote.php
+++ wwwroot/inc/remote.php
@@ -476,6 +476,7 @@ function queryTerminal ($object_id, $commands, $tolerate_remote_errors = TRUE)
 	switch ($breed = detectDeviceBreed ($object_id))
 	{
 		case 'ios12':
+		case 'ios15':
 		case 'air12':
 		case 'ftos8':
 			$protocol = 'netcat'; // default is netcat mode
@@ -546,9 +547,6 @@ function queryTerminal ($object_id, $commands, $tolerate_remote_errors = TRUE)
 			$prompt = '(Login|[Uu]sername|[Pp]assword): $|Press any key to continue(\e\[\??\d+(;\d+)*[A-Za-z])*$|[#>].*$';
 			# Console emulator should be set to 'none' to avoid special characters to be sent by stupid default VT100!!!
 			$commands = "configure\nconsole local-terminal none\nexit\nno page\n" . $commands . "configure\nconsole local-terminal vt100\nend\nexit\nexit\ny\n";
-			break;
-		case 'ios15':
-			$commands = "terminal length 0\nterminal no monitor\n" . $commands;
 			break;
 	}
 	if (! isset ($protocol))
